843d7850a03b7237265aaf908bc29249df8ae760
Rodrez <fabian.rodrez@gmail.com>
2021-07-03 16:07:05 -0400

    Updated rolodex app. Created Card and Card List components

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,39 +1,42 @@
-import React, {Component} from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+// Import our Component
+import { CardList } from './components/card-list/card-list.component'
 
-class App extends Component{
-  constructor(){
-    super();
 
+class App extends Component {
+  // Enable us to use this.state
+  constructor() {
+    super()
+
+    // The state of our component, in this case empty array
     this.state = {
-      string: "Hello Fabian"
+      monsters: []
     }
   }
-  checkText = (text) => {
-    if (text.includes("Fabian")){
-        return "Hello World"
-    }else{
-      return "Hello Fabian"
-    }
-}
 
-  render(){
+
+  // Runs when the components gets mounted to web page
+  componentDidMount(){
+    // Gets api
+    fetch('https://jsonplaceholder.typicode.com/users')
+    // Gets the response from the API
+    .then(response => response.json())
+    // Grabs the user from the API and set the empty state to the users.
+    .then(users => this.setState({monsters: users}))
+    
+  }
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button onClick={()=>this.setState({string: this.checkText(this.state.string)})}>
-            {this.state.string}
-          </button>
-        </header>
+      <div className='App'>
+        {/* Card List Component to wrap our new array (from map) */}
+        <CardList monsters={this.state.monsters}>
+        
+        </CardList>
       </div>
-    );
+    )
   }
 }
 
-
-export default App;
+export default App
